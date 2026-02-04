40a0a80d24608b55a24e192df42dee4ca6749e2e
Rinkal Adesara <167286606+RinkalAdesaraTops@users.noreply.github.com>
2026-02-04 16:50:41 +0530

    mig

--- database/migrations/2026_01_06_101307_create_categories_table.php
+++ database/migrations/2026_01_06_101307_create_categories_table.php
@@ -8,19 +8,20 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
-     */
+    */
     public function up(): void
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->text('catname');
+            $table->text('image');
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
-     */
+    */
     public function down(): void
     {
         Schema::dropIfExists('categories');
