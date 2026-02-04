f50de0c256da751710349a99ba320e269c100ea6
Rinkal Adesara <167286606+RinkalAdesaraTops@users.noreply.github.com>
2026-02-04 16:49:30 +0530

    cat

--- app/Http/Controllers/category.php
+++ app/Http/Controllers/category.php
@@ -8,9 +8,6 @@ use Illuminate\Http\Request;
 
 class category extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $categories = \App\Models\category::get();
@@ -20,21 +17,22 @@ class category extends Controller
         'catdata'=> $categories]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        $data = \App\Models\category::create(['catname'=>$request->catname]);
-        return redirect('/category');
+        $request->validate([
+            'catname'=>'required|min:3',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif',
+        ],[
+            'catname.required'=>'Please enter category',
+            'catname.min'=>'Atleast 3 character should be enter'
+        ]);
+       //img name generate
+        $imgName = "img".time().".".$request->image->extension();
+        $request->image->move(public_path("catimages"), $imgName);
+        $data = \App\Models\category::create(['catname'=>$request->catname,
+            'image'=>$imgName    
+        ]);
+        return redirect('/category')->with('success','Category save successfully');
     }
 
     /**
@@ -57,7 +55,6 @@ class category extends Controller
             'catdata'=> $categories
             ]);
     }
-
     /**
      * Update the specified resource in storage.
      */
@@ -65,16 +62,15 @@ class category extends Controller
     {
         $data = \App\Models\category::findOrFail($id);
         $data->update(['catname'=>$request->catname]);
-        return redirect('/category');
+        return redirect('/category')->with('success',value: 'Category update successfully');
     }
-
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         $data = \App\Models\category::findOrFail($id);
+        if(file_exists(public_path('catimages/'. $data->image))){
+            unlink(public_path('catimages/'. $data->image));
+        }
         $data->delete();
-        return redirect('/category');
+        return redirect('/category')->with('success',value: 'Category delete successfully');
     }
 }
